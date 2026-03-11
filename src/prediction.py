4c0f9f3fdca0888ddba3029da32ca39200c32aed
nehaldhaka <nehalmaria1718@gmail.com>
2026-03-11 12:14:38 +0530

    Update prediction.py

--- src/prediction.py
+++ src/prediction.py
@@ -4,16 +4,24 @@
 
 import pickle
 import numpy as np
+
 class Insurance_prediction:
+
     def __init__(self):
         with open("artifacts/scaler.pkl","rb") as f:
-            self.scaler=pickle.load(f)
+            self.scaler = pickle.load(f)
+
         with open("artifacts/model.pkl","rb") as f:
-            self.model=pickle.load(f)
-    def prediction(self,Age,Annual_Income_LPA,Policy_Term_Years,Sum_Assured_Lakhs):
-        input=np.array([[Age,Annual_Income_LPA,Policy_Term_Years,Sum_Assured_Lakhs]])
-        scaled_input=self.scaler.transform(input)
-        result=self.model.predict(scaled_input)
+            self.model = pickle.load(f)
+
+    def prediction(self, Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs):
+
+        input = np.array([[Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs]])
+
+        scaled_input = self.scaler.transform(input)
+
+        result = self.model.predict(scaled_input)
+
         return result[0]
 
 
